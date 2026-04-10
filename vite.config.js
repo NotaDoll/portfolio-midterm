1522ad70590d28cbc45f0181dedd30add0484396
Barbie Royo <royobarbie682@gmail.com>
2026-04-10 19:36:15 +0800

    add GitHub Actions workflow for deployment to GitHub Pages; remove Tailwind CSS plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue()],
+  base: '/portfolio-midterm/', 
 })
